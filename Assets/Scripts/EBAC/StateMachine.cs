bebd71c94d4b627e7c0c6eb1823a7f7e20531f80
Felipe Farias <felipeo.farias@hotmail.com>
2023-01-29 16:44:39 -0300

    Commit_1

---
+++ Assets/Scripts/EBAC/StateMachine.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+/*ublic class Test
+{
+    public enum Test2
+    {
+        NONE
+    }
+    public void Aa()
+    {
+        StateMachine<Test2> stateMachine = new StateMachine<Test2>();
+        stateMachine.RegisterStates(Test.Test2.NONE, new StateBase());
+    }
+
+}*/
+namespace Ebac.StateMachine
+{
+    public class StateMachine<T> where T : System.Enum
+    {
+        public Dictionary<T, StateBase> dicionaryState;
+        private StateBase _currentstate;
+        public float timeToStartGame = 1f;
+
+        //chave
+        //public Dictionary<StateType, StateBase> dictionaryState;
+        //public static StateMachine Instance;
+
+        public StateBase CurrentState
+        {
+            get { return _currentstate; }
+        }
+        public void Init()
+        {
+            dicionaryState = new Dictionary<T, StateBase>();
+        }
+        public void RegisterStates(T typeEnum, StateBase state)
+        {
+            dicionaryState.Add(typeEnum, state);
+        }
+        public void SwitchState(T state)
+        {
+            if (_currentstate != null) _currentstate.OnStateExit();
+            _currentstate = dicionaryState[state];
+            _currentstate.OnStateEnter();
+        }
+
+
+        public void Update()
+        {
+            if (_currentstate != null) _currentstate.OnStateStay();
+        }
+
+        /*private void Awake()
+        {
+            dictionaryState = new Dictionary<StateType, StateBase>();
+            dictionaryState.Add(StateType.NONE, new StateBase());
+            SwitchState(StateType.NONE);
+            Invoke(nameof(StartGame), timeToStartGame);
+        }
+        [Button]
+        private void StartGame()
+        {
+            SwitchState(StateType.NONE);
+        }
+        public void SwitchState(StateType state)
+        {
+            if (_currentstate != null) _currentstate.OnStateExit();
+            _currentstate = dictionaryState[state];
+            _currentstate.OnStateEnter();
+        }
+    #if UNITY_EDITOR
+    # region DEBUG
+        [Button]
+        private void ChangeStateToStarteX()
+        {
+            SwitchState(StateType.NONE);
+        }
+        [Button]
+        private void ChangeStateToStarteY()
+        {
+            SwitchState(StateType.NONE);
+        }
+        #endregion
+    #endif
+        private void Update()
+        {
+            if (_currentstate != null) _currentstate.OnStateStay();
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                //SwitchState(StateType.DEAD);
+            }
+        }
+
+        /*public void ResetPosition()
+        {
+            SwitchState(StateType.RESET_POSITION);
+        }*/
+    }
+}
+
---
+++ Assets/Scripts/StateMachineEditor.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+
+[CustomEditor(typeof(FSMExample))]
+public class StateMachineEditor : Editor
+{
+    public bool showFoldout;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        FSMExample fsm = (FSMExample)target;
+
+        EditorGUILayout.Space(30);
+        EditorGUILayout.LabelField("State Machine");
+        //
+        if (fsm.stateMachine == null) return;
+        if (fsm.stateMachine.CurrentState != null)
+            EditorGUILayout.LabelField("Current State:", fsm.stateMachine.CurrentState.ToString());
+
+        showFoldout = EditorGUILayout.Foldout(showFoldout, "Avaiable States");
+
+        if(showFoldout)
+        {
+            if (fsm.stateMachine.dicionaryState != null)
+            {
+                var vals = fsm.stateMachine.dicionaryState.Values.ToArray();
+                var keys = fsm.stateMachine.dicionaryState.Keys.ToArray();
+
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    EditorGUILayout.LabelField(string.Format("{0} :: {0}", keys[i], vals[i]));
+                }
+
+            }
+        }
+
+    }
+}
